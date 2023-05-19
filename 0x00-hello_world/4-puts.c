bdc286f0ceb3966e53eeb994984c08121d667d9b
myusername <myemail@gmail.com>
2023-05-19 12:10:21 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 # include<stdio.h>
 /**
-* main - main entry point
-* return : 1 if success
-*/
+ * main - main entry point
+ * return: always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
